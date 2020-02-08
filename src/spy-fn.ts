386021fe40aa3964ef0c2dc64eeab878fd707da2
bouzuya <m@bouzuya.net>
2020-02-08 10:05:19 +0900

    fix: fix eslint errors

--- src/spy-fn.ts
+++ src/spy-fn.ts
@@ -1,14 +1,18 @@
 type SpyFn = {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   calls: any[][];
 };
 
 const spyFn = <T extends Function>(f: T): SpyFn & T => {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const calls: any[][] = [];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const spy = (...args: any[]): any => {
     calls.push(args);
-    return f.apply(null, args);
+    return f(...args);
   };
   Object.defineProperty(spy, "calls", { value: calls });
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return spy as any;
 };
 
